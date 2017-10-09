18683b9996b3b790c9c952210805661525a17aff
Marcelo Cenerino <marcelocenerine@gmail.com>
2017-10-09 22:17:00 +0100

    rename types and variables

--- collections/src/main/scala/strawman/collection/TupleZipped.scala
+++ collections/src/main/scala/strawman/collection/TupleZipped.scala
@@ -2,280 +2,285 @@ package strawman.collection
 
 import scala.{Boolean, StringContext, Unit}
 
-final class Tuple2Zipped[+A, +B] private[collection](as: Iterable[A], bs: Iterable[B]) {
+final class Tuple2Zipped[+El1, +El2] private[collection](coll1: Iterable[El1], coll2: Iterable[El2]) {
 
-  def lazyZip[C](that: Iterable[C]): Tuple3Zipped[A, B, C] = new Tuple3Zipped(as, bs, that)
+  def lazyZip[B](that: Iterable[B]): Tuple3Zipped[El1, El2, B] = new Tuple3Zipped(coll1, coll2, that)
 
-  def map[C](f: (A, B) => C): Iterable[C] = {
-    val builder = as.iterableFactory.newBuilder[C]()
-    val ait = as.iterator()
-    val bit = bs.iterator()
+  def map[B](f: (El1, El2) => B): Iterable[B] = {
+    val b = coll1.iterableFactory.newBuilder[B]()
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
 
-    while (ait.hasNext && bit.hasNext) builder += f(ait.next(), bit.next())
+    while (elems1.hasNext && elems2.hasNext) b += f(elems1.next(), elems2.next())
 
-    builder.result()
+    b.result()
   }
 
-  def flatMap[C](f: (A, B) => Iterable[C]): Iterable[C] = {
-    val builder = as.iterableFactory.newBuilder[C]()
-    val ait = as.iterator()
-    val bit = bs.iterator()
+  def flatMap[B](f: (El1, El2) => Iterable[B]): Iterable[B] = {
+    val b = coll1.iterableFactory.newBuilder[B]()
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
 
-    while (ait.hasNext && bit.hasNext) builder ++= f(ait.next(), bit.next())
+    while (elems1.hasNext && elems2.hasNext) b ++= f(elems1.next(), elems2.next())
 
-    builder.result()
+    b.result()
   }
 
-  def filter(p: (A, B) => Boolean): (Iterable[A], Iterable[B]) = {
-    val builder1 = as.iterableFactory.newBuilder[A]()
-    val builder2 = bs.iterableFactory.newBuilder[B]()
-    val ait = as.iterator()
-    val bit = bs.iterator()
+  def filter(p: (El1, El2) => Boolean): (Iterable[El1], Iterable[El2]) = {
+    val b1 = coll1.iterableFactory.newBuilder[El1]()
+    val b2 = coll2.iterableFactory.newBuilder[El2]()
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
 
-    while (ait.hasNext && bit.hasNext) {
-      val a = ait.next()
-      val b = bit.next()
-      if (p(a, b)) {
-        builder1 += a
-        builder2 += b
+    while (elems1.hasNext && elems2.hasNext) {
+      val el1 = elems1.next()
+      val el2 = elems2.next()
+      if (p(el1, el2)) {
+        b1 += el1
+        b2 += el2
       }
     }
 
-    (builder1.result(), builder2.result())
+    (b1.result(), b2.result())
   }
 
-  def exists(p: (A, B) => Boolean): Boolean = {
-    val ait = as.iterator()
-    val bit = bs.iterator()
+  def exists(p: (El1, El2) => Boolean): Boolean = {
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
     var res = false
 
-    while (!res && ait.hasNext && bit.hasNext) res = p(ait.next(), bit.next())
+    while (!res && elems1.hasNext && elems2.hasNext) res = p(elems1.next(), elems2.next())
 
     res
   }
 
-  def forall(p: (A, B) => Boolean): Boolean = !exists((a, b) => !p(a, b))
+  def forall(p: (El1, El2) => Boolean): Boolean = !exists((el1, el2) => !p(el1, el2))
 
-  def foreach[U](f: (A, B) => U): Unit = {
-    val ait = as.iterator()
-    val bit = bs.iterator()
+  def foreach[U](f: (El1, El2) => U): Unit = {
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
 
-    while (ait.hasNext && bit.hasNext) f(ait.next(), bit.next())
+    while (elems1.hasNext && elems2.hasNext) f(elems1.next(), elems2.next())
   }
 
-  private def iterator(): Iterator[(A, B)] = new Iterator[(A, B)] {
-    val ait = as.iterator()
-    val bit = bs.iterator()
+  private def iterator(): Iterator[(El1, El2)] = new Iterator[(El1, El2)] {
+    private val elems1 = coll1.iterator()
+    private val elems2 = coll2.iterator()
 
-    override def hasNext: Boolean = ait.hasNext && bit.hasNext
-    override def next(): (A, B) = (ait.next(), bit.next())
+    override def hasNext: Boolean = elems1.hasNext && elems2.hasNext
+    override def next(): (El1, El2) = (elems1.next(), elems2.next())
   }
 
   def className = getClass.getName
 
-  override def toString = s"$className($as, $bs)"
+  override def toString = s"$className($coll1, $coll2)"
 }
 
 object Tuple2Zipped {
-  implicit def tuple2ZippedToIterable[A, B](zipped2: Tuple2Zipped[A, B]): Iterable[(A, B)] = new View[(A, B)] {
-    override def iterator(): Iterator[(A, B)] = zipped2.iterator()
+  implicit def tuple2ZippedToIterable[El1, El2](zipped2: Tuple2Zipped[El1, El2]): Iterable[(El1, El2)] = new View[(El1, El2)] {
+    override def iterator(): Iterator[(El1, El2)] = zipped2.iterator()
   }
 }
 
 
 
-final class Tuple3Zipped[+A, +B, +C] private[collection](as: Iterable[A], bs: Iterable[B], cs: Iterable[C]) {
+final class Tuple3Zipped[+El1, +El2, +El3] private[collection](coll1: Iterable[El1], coll2: Iterable[El2], coll3: Iterable[El3]) {
 
-  def lazyZip[D](that: Iterable[D]): Tuple4Zipped[A, B, C, D] = new Tuple4Zipped(as, bs, cs, that)
+  def lazyZip[B](that: Iterable[B]): Tuple4Zipped[El1, El2, El3, B] = new Tuple4Zipped(coll1, coll2, coll3, that)
 
-  def map[D](f: (A, B, C) => D): Iterable[D] = {
-    val builder = as.iterableFactory.newBuilder[D]()
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
+  def map[B](f: (El1, El2, El3) => B): Iterable[B] = {
+    val b = coll1.iterableFactory.newBuilder[B]()
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
+    val elems3 = coll3.iterator()
 
-    while (ait.hasNext && bit.hasNext && cit.hasNext)
-      builder += f(ait.next(), bit.next(), cit.next())
+    while (elems1.hasNext && elems2.hasNext && elems3.hasNext)
+      b += f(elems1.next(), elems2.next(), elems3.next())
 
-    builder.result()
+    b.result()
   }
 
-  def flatMap[D](f: (A, B, C) => Iterable[D]): Iterable[D] = {
-    val builder = as.iterableFactory.newBuilder[D]()
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
+  def flatMap[B](f: (El1, El2, El3) => Iterable[B]): Iterable[B] = {
+    val b = coll1.iterableFactory.newBuilder[B]()
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
+    val elems3 = coll3.iterator()
 
-    while (ait.hasNext && bit.hasNext && cit.hasNext)
-      builder ++= f(ait.next(), bit.next(), cit.next())
+    while (elems1.hasNext && elems2.hasNext && elems3.hasNext)
+      b ++= f(elems1.next(), elems2.next(), elems3.next())
 
-    builder.result()
+    b.result()
   }
 
-  def filter(p: (A, B, C) => Boolean): (Iterable[A], Iterable[B], Iterable[C]) = {
-    val builder1 = as.iterableFactory.newBuilder[A]()
-    val builder2 = bs.iterableFactory.newBuilder[B]()
-    val builder3 = cs.iterableFactory.newBuilder[C]()
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
+  def filter(p: (El1, El2, El3) => Boolean): (Iterable[El1], Iterable[El2], Iterable[El3]) = {
+    val b1 = coll1.iterableFactory.newBuilder[El1]()
+    val b2 = coll2.iterableFactory.newBuilder[El2]()
+    val b3 = coll3.iterableFactory.newBuilder[El3]()
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
+    val elems3 = coll3.iterator()
 
-    while (ait.hasNext && bit.hasNext && cit.hasNext) {
-      val a = ait.next()
-      val b = bit.next()
-      val c = cit.next()
-      if (p(a, b, c)) {
-        builder1 += a
-        builder2 += b
-        builder3 += c
+    while (elems1.hasNext && elems2.hasNext && elems3.hasNext) {
+      val el1 = elems1.next()
+      val el2 = elems2.next()
+      val el3 = elems3.next()
+      if (p(el1, el2, el3)) {
+        b1 += el1
+        b2 += el2
+        b3 += el3
       }
     }
 
-    (builder1.result(), builder2.result(), builder3.result())
+    (b1.result(), b2.result(), b3.result())
   }
 
-  def exists(p: (A, B, C) => Boolean): Boolean = {
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
+  def exists(p: (El1, El2, El3) => Boolean): Boolean = {
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
+    val elems3 = coll3.iterator()
     var res = false
 
-    while (!res && ait.hasNext && bit.hasNext && cit.hasNext)
-      res = p(ait.next(), bit.next(), cit.next())
+    while (!res && elems1.hasNext && elems2.hasNext && elems3.hasNext)
+      res = p(elems1.next(), elems2.next(), elems3.next())
 
     res
   }
 
-  def forall(p: (A, B, C) => Boolean): Boolean = !exists((a, b, c) => !p(a, b, c))
+  def forall(p: (El1, El2, El3) => Boolean): Boolean = !exists((el1, el2, el3) => !p(el1, el2, el3))
 
-  def foreach[U](f: (A, B, C) => U): Unit = {
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
+  def foreach[U](f: (El1, El2, El3) => U): Unit = {
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
+    val elems3 = coll3.iterator()
 
-    while (ait.hasNext && bit.hasNext && cit.hasNext)
-      f(ait.next(), bit.next(), cit.next())
+    while (elems1.hasNext && elems2.hasNext && elems3.hasNext)
+      f(elems1.next(), elems2.next(), elems3.next())
   }
 
-  private def iterator(): Iterator[(A, B, C)] = new Iterator[(A, B, C)] {
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
+  private def iterator(): Iterator[(El1, El2, El3)] = new Iterator[(El1, El2, El3)] {
+    private val elems1 = coll1.iterator()
+    private val elems2 = coll2.iterator()
+    private val elems3 = coll3.iterator()
 
-    override def hasNext: Boolean = ait.hasNext && bit.hasNext && cit.hasNext
-    override def next(): (A, B, C) = (ait.next(), bit.next(), cit.next())
+    override def hasNext: Boolean = elems1.hasNext && elems2.hasNext && elems3.hasNext
+    override def next(): (El1, El2, El3) = (elems1.next(), elems2.next(), elems3.next())
   }
 
   def className = getClass.getName
 
-  override def toString = s"$className($as, $bs, $cs)"
+  override def toString = s"$className($coll1, $coll2, $coll3)"
 }
 
 object Tuple3Zipped {
-  implicit def tuple3ZippedToIterable[A, B, C](zipped3: Tuple3Zipped[A, B, C]): Iterable[(A, B, C)] = new View[(A, B, C)] {
-    override def iterator(): Iterator[(A, B, C)] = zipped3.iterator()
+  implicit def tuple3ZippedToIterable[El1, El2, El3](zipped3: Tuple3Zipped[El1, El2, El3]): Iterable[(El1, El2, El3)] =
+    new View[(El1, El2, El3)] {
+      override def iterator(): Iterator[(El1, El2, El3)] = zipped3.iterator()
   }
 }
 
 
 
 
-final class Tuple4Zipped[+A, +B, +C, +D] private[collection](as: Iterable[A], bs: Iterable[B], cs: Iterable[C], ds: Iterable[D]) {
+final class Tuple4Zipped[+El1, +El2, +El3, +El4] private[collection](coll1: Iterable[El1],
+                                                                     coll2: Iterable[El2],
+                                                                     coll3: Iterable[El3],
+                                                                     coll4: Iterable[El4]) {
 
-  def map[E](f: (A, B, C, D) => E): Iterable[E] = {
-    val builder = as.iterableFactory.newBuilder[E]()
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
-    val dit = ds.iterator()
+  def map[B](f: (El1, El2, El3, El4) => B): Iterable[B] = {
+    val b = coll1.iterableFactory.newBuilder[B]()
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
+    val elems3 = coll3.iterator()
+    val elems4 = coll4.iterator()
 
-    while (ait.hasNext && bit.hasNext && cit.hasNext && dit.hasNext)
-      builder += f(ait.next(), bit.next(), cit.next(), dit.next())
+    while (elems1.hasNext && elems2.hasNext && elems3.hasNext && elems4.hasNext)
+      b += f(elems1.next(), elems2.next(), elems3.next(), elems4.next())
 
-    builder.result()
+    b.result()
   }
 
-  def flatMap[E](f: (A, B, C, D) => Iterable[E]): Iterable[E] = {
-    val builder = as.iterableFactory.newBuilder[E]()
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
-    val dit = ds.iterator()
+  def flatMap[B](f: (El1, El2, El3, El4) => Iterable[B]): Iterable[B] = {
+    val b = coll1.iterableFactory.newBuilder[B]()
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
+    val elems3 = coll3.iterator()
+    val elems4 = coll4.iterator()
 
-    while (ait.hasNext && bit.hasNext && cit.hasNext && dit.hasNext)
-      builder ++= f(ait.next(), bit.next(), cit.next(), dit.next())
+    while (elems1.hasNext && elems2.hasNext && elems3.hasNext && elems4.hasNext)
+      b ++= f(elems1.next(), elems2.next(), elems3.next(), elems4.next())
 
-    builder.result()
+    b.result()
   }
 
-  def filter(p: (A, B, C, D) => Boolean): (Iterable[A], Iterable[B], Iterable[C], Iterable[D]) = {
-    val builder1 = as.iterableFactory.newBuilder[A]()
-    val builder2 = bs.iterableFactory.newBuilder[B]()
-    val builder3 = cs.iterableFactory.newBuilder[C]()
-    val builder4 = ds.iterableFactory.newBuilder[D]()
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
-    val dit = ds.iterator()
+  def filter(p: (El1, El2, El3, El4) => Boolean): (Iterable[El1], Iterable[El2], Iterable[El3], Iterable[El4]) = {
+    val b1 = coll1.iterableFactory.newBuilder[El1]()
+    val b2 = coll2.iterableFactory.newBuilder[El2]()
+    val b3 = coll3.iterableFactory.newBuilder[El3]()
+    val b4 = coll4.iterableFactory.newBuilder[El4]()
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
+    val elems3 = coll3.iterator()
+    val elems4 = coll4.iterator()
 
-    while (ait.hasNext && bit.hasNext && cit.hasNext && dit.hasNext) {
-      val a = ait.next()
-      val b = bit.next()
-      val c = cit.next()
-      val d = dit.next()
-      if (p(a, b, c, d)) {
-        builder1 += a
-        builder2 += b
-        builder3 += c
-        builder4 += d
+    while (elems1.hasNext && elems2.hasNext && elems3.hasNext && elems4.hasNext) {
+      val el1 = elems1.next()
+      val el2 = elems2.next()
+      val el3 = elems3.next()
+      val el4 = elems4.next()
+      if (p(el1, el2, el3, el4)) {
+        b1 += el1
+        b2 += el2
+        b3 += el3
+        b4 += el4
       }
     }
 
-    (builder1.result(), builder2.result(), builder3.result(), builder4.result())
+    (b1.result(), b2.result(), b3.result(), b4.result())
   }
 
-  def exists(p: (A, B, C, D) => Boolean): Boolean = {
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
-    val dit = ds.iterator()
+  def exists(p: (El1, El2, El3, El4) => Boolean): Boolean = {
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
+    val elems3 = coll3.iterator()
+    val elems4 = coll4.iterator()
     var res = false
 
-    while (!res && ait.hasNext && bit.hasNext && cit.hasNext && dit.hasNext)
-      res = p(ait.next(), bit.next(), cit.next(), dit.next())
+    while (!res && elems1.hasNext && elems2.hasNext && elems3.hasNext && elems4.hasNext)
+      res = p(elems1.next(), elems2.next(), elems3.next(), elems4.next())
 
     res
   }
 
-  def forall(p: (A, B, C, D) => Boolean): Boolean = !exists((a, b, c, d) => !p(a, b, c, d))
+  def forall(p: (El1, El2, El3, El4) => Boolean): Boolean = !exists((el1, el2, el3, el4) => !p(el1, el2, el3, el4))
 
-  def foreach[U](f: (A, B, C, D) => U): Unit = {
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
-    val dit = ds.iterator()
+  def foreach[U](f: (El1, El2, El3, El4) => U): Unit = {
+    val elems1 = coll1.iterator()
+    val elems2 = coll2.iterator()
+    val elems3 = coll3.iterator()
+    val elems4 = coll4.iterator()
 
-    while (ait.hasNext && bit.hasNext && cit.hasNext && dit.hasNext)
-      f(ait.next(), bit.next(), cit.next(), dit.next())
+    while (elems1.hasNext && elems2.hasNext && elems3.hasNext && elems4.hasNext)
+      f(elems1.next(), elems2.next(), elems3.next(), elems4.next())
   }
 
-  private def iterator(): Iterator[(A, B, C, D)] = new Iterator[(A, B, C, D)] {
-    val ait = as.iterator()
-    val bit = bs.iterator()
-    val cit = cs.iterator()
-    val dit = ds.iterator()
+  private def iterator(): Iterator[(El1, El2, El3, El4)] = new Iterator[(El1, El2, El3, El4)] {
+    private val elems1 = coll1.iterator()
+    private val elems2 = coll2.iterator()
+    private val elems3 = coll3.iterator()
+    private val elems4 = coll4.iterator()
 
-    override def hasNext: Boolean = ait.hasNext && bit.hasNext && cit.hasNext && dit.hasNext
-    override def next(): (A, B, C, D) = (ait.next(), bit.next(), cit.next(), dit.next())
+    override def hasNext: Boolean = elems1.hasNext && elems2.hasNext && elems3.hasNext && elems4.hasNext
+    override def next(): (El1, El2, El3, El4) = (elems1.next(), elems2.next(), elems3.next(), elems4.next())
   }
 
   def className = getClass.getName
 
-  override def toString = s"$className($as, $bs, $cs, $ds)"
+  override def toString = s"$className($coll1, $coll2, $coll3, $coll4)"
 }
 
 object Tuple4Zipped {
-  implicit def tuple4ZippedToIterable[A, B, C, D](zipped4: Tuple4Zipped[A, B, C, D]): Iterable[(A, B, C, D)] = new View[(A, B, C, D)] {
-    override def iterator(): Iterator[(A, B, C, D)] = zipped4.iterator()
+  implicit def tuple4ZippedToIterable[El1, El2, El3, El4](zipped4: Tuple4Zipped[El1, El2, El3, El4]): Iterable[(El1, El2, El3, El4)] =
+    new View[(El1, El2, El3, El4)] {
+      override def iterator(): Iterator[(El1, El2, El3, El4)] = zipped4.iterator()
   }
 }
